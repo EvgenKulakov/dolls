ba649f519b61e932a9c5c1d00784512e22c0f6b2
Evgeniy Kulakov <000evgeniykulakov@gmail.com>
2024-09-15 17:40:02 +0300

    003 add maxVal, nameVal in constructor

--- dolls.ino
+++ dolls.ino
@@ -5,19 +5,21 @@ class Doll {
     int minVal;
     int maxVal;
     uint8_t pin;
+    String name;
 
   public:
-    Doll(int minimumVal, uint8_t pinPotentiometer) {
+    Doll(int minimumVal, int maximumVal, uint8_t pinPotentiometer, String nameVal) {
       minVal = minimumVal;
-      maxVal = minimumVal + 20;
+      maxVal = maximumVal;
       pin = pinPotentiometer;
+      name = nameVal;
 
       pinMode(pin, INPUT);
     }
 
     bool isOpen() {
       int currentVal = analogRead(pin);
-      Serial.println(currentVal);
+      // Serial.println(name + currentVal);
       if (currentVal >= minVal && currentVal <= maxVal) {
         return true;
       }
@@ -25,11 +27,11 @@ class Doll {
     }
 };
 
-Doll I_AM(300, A0);
-Doll VOODOO(400, A1);
-Doll BEAR(500, A2);
-Doll CLOWN(600, A3);
-Doll GIRL(700, A4);
+Doll I_AM(122, 132, A0, "I_AM");
+Doll VOODOO(780, 790, A2, "VOODOO");
+Doll BEAR(560, 570, A4, "BEAR");
+Doll CLOWN(460, 470, A6, "CLOWN");
+Doll GIRL(984, 994, A7, "GIRL");
 
 bool lock = HIGH; // HIGH - lock активен, ящик закрыт
 #define PIN_LOCK 4
@@ -37,16 +39,18 @@ bool lock = HIGH; // HIGH - lock активен, ящик закрыт
 void setup() {
   pinMode(PIN_LOCK, OUTPUT);
   digitalWrite(PIN_LOCK, lock);
-  Serial.begin(9600);
+  // Serial.begin(9600);
 }
 
 void loop() {
   delay(100);
   bool open = I_AM.isOpen() && VOODOO.isOpen() && BEAR.isOpen() && CLOWN.isOpen() && GIRL.isOpen();
-  Serial.println(open);
-  Serial.println(lock);
-  if (open == true && lock == HIGH || open == false && lock == LOW) {
-    lock = !lock;
-    digitalWrite(PIN_LOCK, lock);
+  if (open == lock) {
+    delay(800);
+    bool openAfterPaused = I_AM.isOpen() && VOODOO.isOpen() && BEAR.isOpen() && CLOWN.isOpen() && GIRL.isOpen();
+    if (openAfterPaused == lock) {
+      lock = !lock;
+      digitalWrite(PIN_LOCK, lock);
+    }
   }
 }
